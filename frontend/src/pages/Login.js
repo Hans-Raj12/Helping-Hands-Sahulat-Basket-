7ce00de0fef6099b95156b66f5df1ae88ab01907
Hans Raj <hans63565@gmail.com>
2023-02-28 00:05:58 +0500

    created login page and also added CSS to make it look better

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,33 @@
 const Login = () => {
+
+  
   return (
-    <>
-    <h1>login</h1>
-    </>
+    <div className="form-container">
+      <form>
+        <h3>Log In</h3>
+        <label htmlfor="email" className="form-label">Email: </label>
+        <input
+          type='email'
+          onChange={e => e.target.value}
+          id='email'
+          placeholder='Enter your email here'
+        />
+        <label htmlfor='password' className="form-label">Password: </label>
+        <input
+          type='password'
+          onChange={e => e.target.value}
+          id="password"
+          placeholder='Enter password'
+        />
+        <input
+          id='cbox'
+          type='checkbox'
+          className=""
+        />
+        <label>Remember Me</label>
+        <button>Log In</button>
+      </form>
+    </div>
   );
 }
 
